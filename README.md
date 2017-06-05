882e234f0824c0e193d231c096c9585ba69d4b95
Phil Glover <pglover@companieshouse.gov.uk>
2017-06-05 15:34:53 +0100

    removing some warns, renaming variables and improving messages
    - adding a metric to units json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unit-conversion
 C1 technical exercise, measurement conversion based on Jess's units.json
+
+change convert.pl permissions to be executable
+
+usage for example
+./convert.pl --type=weight --from=lb --to=kg --v=140
--- convert.pl
+++ convert.pl
@@ -15,36 +15,45 @@ GetOptions(
     'from=s' => \$from,
     'to=s'   => \$to,
     'v=f'    => \$value,
-) or die ("arggh");
+) or exit usage();
 
-#print "type : $type\n";
-#print "from : $from\n";
-#print "to : $to\n";
-#print "value : $value\n";
+sub usage {
+	print <<EOS;
+./convert.pl --type --from --to --v
 
-my $units_json;
+Converts measurement units as per given json file.
+
+options:
+    -type=s		unit of measurement eg time
+    -from=s     convert from this unit
+    -to=s       convert to this unit
+    -v=f       	value to for filtering, see docs
+    -help       print this help
+
+EOS
+    return 1;
+}
+
+#my $units.json = get https://gist.githubusercontent.com/jessjenkins/c88c6ff207bc43721e729f9c9011aafa/raw/2baa8d9ffe309632d8b55107caffd745d1802651/units.json
+my $unitsJSON;
 {
     local $/;
     open my $fh, "<", "units.json" or die "Can't open file";
-    $units_json = <$fh>;
+    $unitsJSON = <$fh>;
     close $fh;
 }
-#warn Dumper \$units_json;
-my $conversion = decode_json($units_json);
-#warn Dumper $conversion;
+
+my $unitsHASH = decode_json($unitsJSON);
+#warn Dumper $unitsHASH;
 my %lookup;
-foreach my $unit (@$conversion) {
-    #warn Dumper $unit->{name};
-    #$lookup{$unit->{name}} = 1;
+foreach my $unit (@$unitsHASH) {
     $lookup{$unit->{name}} = $unit->{units};
 }
 #warn "lookup " . Dumper \%lookup;
-#warn "one " . $lookup{'time'}->{min};
 
 die "The '$type' measurement can't be converted, please see https://gist.github.com/jessjenkins/c88c6ff207bc43721e729f9c9011aafa\n" if (!defined $lookup{$type});
 die "The '$from' unit isn't part of '$type', please see https://gist.github.com/jessjenkins/c88c6ff207bc43721e729f9c9011aafa\n" if (!defined $lookup{$type}->{$from});
 die "The '$to' unit isn't part of '$type', please see https://gist.github.com/jessjenkins/c88c6ff207bc43721e729f9c9011aafa\n" if (!defined $lookup{$type}->{$to});
 
 my $answer = $value * $lookup{$type}->{$from} * 1/$lookup{$type}->{$to};
-
-print "The answer is $answer\n";
+print "The conversion of $value$from in $type is $answer$to.\n";
